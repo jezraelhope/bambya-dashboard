ccccd592efa5d7e07a36e1478bc3bdb5577f46f0
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-09-19 16:57:10 -0500

    Fetch data from API

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,19 @@ import React, { useState, useEffect} from "react";
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
-import { fetchedData } from "./data/utils";
 
-
+const fetchedData = async () => {
+	try {
+		const data = await fetch("/trades").then(res => res.json()).then(data => data)
+        return data
+	} catch(err) {
+		console.log(err)
+	}
+}
 
 
 function App(){
 	const [monthToShow, setMonthToShow] = useState([]);
-	
 	const [data, setData] = useState([]);
     
     useEffect(async () => {
@@ -18,11 +23,10 @@ function App(){
 		setData(fetched);
       }, [setData]);
 
-	  
 	return (
 		<div className="App">
-			<Header setMonthToShow={setMonthToShow} />
-			<Body monthToShow={monthToShow}/>
+			<Header setMonthToShow={setMonthToShow} data={data} />
+			<Body monthToShow={monthToShow} data={data}/>
 			<Footer />
 		</div>
 	);
