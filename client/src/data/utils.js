9deffb342e6f2888e9aabf6e0196ff6504cb6c1f
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-09-27 20:56:06 -0500

    Remove monthWords

--- client/src/data/utils.js
+++ client/src/data/utils.js
@@ -1,19 +1,3 @@
 import newSampleData from "./newSampleData";
 
 export const months = Object.keys(newSampleData);
-
-export const monthWords = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December"
-}
-
