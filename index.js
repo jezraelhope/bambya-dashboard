5a6522710ef34b8101378e29a890aae9c7519873
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-25 13:54:18 -0600

    remove commented out code

--- index.js
+++ index.js
@@ -3,24 +3,24 @@
 //+++++++++++++++++++++++++++++++++++++
 
 //NPM Imports
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 // const expressSession = require("express-session");
 
 //Config Import
-const config = require("./config");
+const config = require('./config')
 
 //Model Imports
-const Trade = require("./models/trades");
-const User = require("./models/user");
+const Trade = require('./models/trades')
+const User = require('./models/user')
 
 //Route Imports
-const tradeRoutes = require("./routes/trade");
-const closetradeRoutes = require("./routes/closeTrade");
-const authRoutes = require("./routes/auth");
+const tradeRoutes = require('./routes/trade')
+const closetradeRoutes = require('./routes/closeTrade')
+const authRoutes = require('./routes/auth')
 // const mainRoutes = require('./routes/trade')
 
 //+++++++++++++++++++++++++++++++++++++
@@ -29,51 +29,34 @@ const authRoutes = require("./routes/auth");
 
 //Connect to DB
 mongoose.connect(config.db.connection, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 //Express Config
-app.use(express.static("./client/build"));
+app.use(express.static('./client/build'))
 app.use(
-	express.json({
-		type: ["application/json", "text/plain"],
-	})
-);
-
-// //Express Session Config
-
-// app.use(
-// 	expressSession({
-// 		secret: "jdshfjdhldmfsdjfksdhjfkdssdfksdj sdofjp[ioie -394r sdfy989y3q dfh8ye",
-// 		resave: false,
-// 		saveUninitialized: false,
-// 	})
-// );
-
-// //Passport config
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-// passport.use(new LocalStrategy(User.authenticate()));
+    express.json({
+        type: ['application/json', 'text/plain'],
+    })
+)
 
 //Route Config
 //app.use("/", mainRoutes)
-app.use("/", authRoutes);
-app.use("/trades", tradeRoutes);
-app.use("/close", closetradeRoutes);
+app.use('/', authRoutes)
+app.use('/trades', tradeRoutes)
+app.use('/close', closetradeRoutes)
 
 //Main Routes
-app.get("/", (req, res) => {
-	res.json({ message: "The connection is working!!!" });
-});
+app.get('/', (req, res) => {
+    res.json({ message: 'The connection is working!!!' })
+})
 //+++++++++++++++++++++++++++++++++++++
 //Listen
 //+++++++++++++++++++++++++++++++++++++
 
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 9001
 
 app.listen(port, () => {
-	console.log(`App listening on port ${port}`);
-});
+    console.log(`App listening on port ${port}`)
+})
