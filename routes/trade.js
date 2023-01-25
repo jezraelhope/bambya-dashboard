df493cd9dcdd2c0a4c9d75e69c72ac9181d3acd5
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-25 13:57:13 -0600

    removed commented out code

--- routes/trade.js
+++ routes/trade.js
@@ -1,65 +1,43 @@
-const express = require('express');
-const router = express.Router();
-const Trade = require('../models/trades');
-const request = require('request');
-const config = require('../config');
+const express = require('express')
+const router = express.Router()
+const Trade = require('../models/trades')
+const request = require('request')
+const config = require('../config')
 
 const monthWords = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December"
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
 }
 
-// //Alpha Advantage Config
-
-// let ticker = "TSLA";
-// let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${config.alphaAdvantageAPIKey}`
-
-// request.get({
-//     url: url,
-//     json: true,
-//     headers: {'User-Agent': 'request'}
-//   }, (err, res, data) => {
-//     if (err) {
-//       console.log('Error:', err);
-//     } else if (res.statusCode !== 200) {
-//       console.log('Status:', res.statusCode);
-//     } else {
-//       // data is successfully parsed as a JSON object:
-//       console.log(data);
-//     }
-// });
-
 //Index
-router.get("/", async (req, res) => {
-    try{
-        const trades = await Trade.find().exec();
+router.get('/', async (req, res) => {
+    try {
+        const trades = await Trade.find().exec()
         const refactoredTrades = trades.reduce((acc, trade) => {
             const d = new Date(trade.expiryDate)
-           
-            year =  d.getUTCFullYear();
-            month = d.getUTCMonth()+1;
-            day = d.getUTCDate();
-        
-            
 
-            if(!acc[year]) {
-                acc[year] = {};
+            year = d.getUTCFullYear()
+            month = d.getUTCMonth() + 1
+            day = d.getUTCDate()
+
+            if (!acc[year]) {
+                acc[year] = {}
             }
-            if(!acc[year][monthWords[month]]) {
-                acc[year][monthWords[month]] = {};
+            if (!acc[year][monthWords[month]]) {
+                acc[year][monthWords[month]] = {}
             }
-            if(!acc[year][monthWords[month]][day]) {
-                acc[year][monthWords[month]][day] = [];
+            if (!acc[year][monthWords[month]][day]) {
+                acc[year][monthWords[month]][day] = []
             }
             acc[year][monthWords[month]][day].push({
                 symbol: trade.sym,
@@ -71,11 +49,11 @@ router.get("/", async (req, res) => {
                 comments: trade.openComments,
                 id: trade._id,
                 date: trade.expiryDate,
-                closingData: trade.closingData
+                closingData: trade.closingData,
             })
             return acc
         }, {})
-        
+
         res.send(refactoredTrades)
     } catch (err) {
         console.log(err)
@@ -83,7 +61,7 @@ router.get("/", async (req, res) => {
 })
 
 //Create
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
     const newTrade = {
         sym: req.body.symbol,
         expiryDate: req.body.expiryDate,
@@ -93,20 +71,20 @@ router.post("/", async (req, res) => {
         contractsNumber: req.body.contractsNumber,
         openPrice: req.body.openPrice,
         openComments: req.body.openComments,
-        closingData: {}
+        closingData: {},
     }
-    
+
     try {
         const trade = await Trade.create(newTrade)
         req.redirect('/trades/${trade._id}')
-    } catch(e) {
-        res.redirect("/trades")
+    } catch (e) {
+        res.redirect('/trades')
     }
 })
 
 //Edit
 
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
     const tradeBody = {
         sym: req.body.symbol,
         expiryDate: req.body.expiryDate,
@@ -118,7 +96,11 @@ router.put("/:id", async (req, res) => {
         openComments: req.body.openComments,
     }
     try {
-        const editedTrade = await Trade.findByIdAndUpdate(req.params.id, tradeBody, {new:true}).exec();
+        const editedTrade = await Trade.findByIdAndUpdate(
+            req.params.id,
+            tradeBody,
+            { new: true }
+        ).exec()
         res.send(editedTrade)
         console.log(editedTrade)
     } catch (e) {
@@ -126,16 +108,14 @@ router.put("/:id", async (req, res) => {
     }
 })
 
-
-
 //Delete
-router.delete("/:id", async (req,res) => {
+router.delete('/:id', async (req, res) => {
     try {
-        const deletedTrade = await Trade.findByIdAndDelete(req.params.id).exec();
+        const deletedTrade = await Trade.findByIdAndDelete(req.params.id).exec()
         res.send(deletedTrade)
     } catch (err) {
-        res.redirect("back")
+        res.redirect('back')
     }
 })
 
-module.exports = router;
+module.exports = router
