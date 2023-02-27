78f8e228dd833ef6a6b0c5acf5378b8e586d4368
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-02-27 16:54:08 -0600

    Fix filter button sstyling

--- client/src/Body/index.js
+++ client/src/Body/index.js
@@ -12,14 +12,9 @@ const Body = (props) => {
     const changeTradeView = (e) => {
         setTradeView(e.target.value)
     }
-
-    return (
-        <main className="main-container">
-            {(monthToShow || []).length
-                ? null
-                : 'Please choose the month from the left side menu to filter'}
-
-            <div>
+    const main = () => {
+        if ((monthToShow || []).length) {
+            return (
                 <div className="open-closed-filter-container">
                     <button
                         onClick={changeTradeView}
@@ -43,32 +38,40 @@ const Body = (props) => {
                         closed trades
                     </button>
                 </div>
-                {monthToShow.map((month) => {
-                    return (
-                        <div className="monthly-container" key={month}>
-                            <h2 className="month-name">{month}</h2>
-                            <MonthlyComponent
-                                month={month}
-                                year={yearSelected}
-                                data={data}
-                                hideAllMenus={props.hideAllMenus}
-                                setHideAllMenus={props.setHideAllMenus}
-                                handleDelete={props.handleDelete}
-                                setModalVisibility={props.setModalVisibility}
-                                setEditFormVisibility={
-                                    props.setEditFormVisibility
-                                }
-                                setCloseTradeFormVisibility={
-                                    props.setCloseTradeFormVisibility
-                                }
-                                setTradeData={props.setTradeData}
-                                setRefetch={props.setRefetch}
-                                tradeView={tradeView}
-                            />
-                        </div>
-                    )
-                })}
-            </div>
+            )
+        } else {
+            return (
+                <p>Please choose the month from the left side menu to filter</p>
+            )
+        }
+    }
+
+    return (
+        <main className="main-container">
+            {main()}
+            {monthToShow.map((month) => {
+                return (
+                    <div className="monthly-container" key={month}>
+                        <h2 className="month-name">{month}</h2>
+                        <MonthlyComponent
+                            month={month}
+                            year={yearSelected}
+                            data={data}
+                            hideAllMenus={props.hideAllMenus}
+                            setHideAllMenus={props.setHideAllMenus}
+                            handleDelete={props.handleDelete}
+                            setModalVisibility={props.setModalVisibility}
+                            setEditFormVisibility={props.setEditFormVisibility}
+                            setCloseTradeFormVisibility={
+                                props.setCloseTradeFormVisibility
+                            }
+                            setTradeData={props.setTradeData}
+                            setRefetch={props.setRefetch}
+                            tradeView={tradeView}
+                        />
+                    </div>
+                )
+            })}
         </main>
     )
 }
